cb12e02cbb8f31b6f90d648ca806a96bda9eb0cc
Rachid Snoussi <rsnoussi@redhat.com>
2019-01-24 17:19:47 +0100

    updated for RHDM 7.2

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Quick Loan Bank - UI
 
-Quick Loan Bank UI is an example demo invoking a decision service based on [Red Hat Decision Manager 7](https://www.redhat.com/en/technologies/jboss-middleware/businessrules).
+Quick Loan Bank UI is an example demo invoking a decision service based on [Red Hat Decision Manager .2](https://www.redhat.com/en/technologies/jboss-middleware/businessrules).
 
-![qlb rhdm 7 demo](img/qlb_rhdm.png?raw=true)
+![qlb rhdm 7.2 demo](img/qlb_rhdm.png?raw=true)
 
 The Quick Loan Bank UI is based on [AngularJS](https://angularjs.org) and [PatternFly](http://patternfly.org)
 
@@ -11,22 +11,20 @@ The Quick Loan Bank UI is based on [AngularJS](https://angularjs.org) and [Patte
 ## Prerequisites
 
 1. Enable Cross-Origin Resource Sharing (CORS) on the server where the Loan Application Decision Service is deployed
-For EAP 7.1, add or replace existing undertow in your configuration file:
-
-        <subsystem xmlns="urn:jboss:domain:undertow:4.0">
+For EAP 7.2, add or replace existing undertow in your configuration file:
+```
+        <subsystem xmlns="urn:jboss:domain:undertow:7.0" default-server="default-server" default-virtual-host="default-host" default-servlet-container="default" default-security-domain="other">
             <buffer-cache name="default"/>
             <server name="default-server">
                 <http-listener name="default" socket-binding="http" redirect-socket="https" enable-http2="true"/>
                 <https-listener name="https" socket-binding="https" security-realm="ApplicationRealm" enable-http2="true"/>
                 <host name="default-host" alias="localhost">
                     <location name="/" handler="welcome-content"/>
-                    <filter-ref name="server-header"/>
-                    <filter-ref name="x-powered-by-header"/>
 
                     <filter-ref name="Access-Control-Allow-Origin"/>
                     <filter-ref name="Access-Control-Allow-Methods"/>
                     <filter-ref name="Access-Control-Allow-Headers"/>                                        
-                    <filter-ref name="Access-Control-Allow-Credentials"/>   
+                    <filter-ref name="Access-Control-Allow-Credentials"/>  
 
                     <http-invoker security-realm="ApplicationRealm"/>
                 </host>
@@ -38,18 +36,16 @@ For EAP 7.1, add or replace existing undertow in your configuration file:
             <handlers>
                 <file name="welcome-content" path="${jboss.home.dir}/welcome-content"/>
             </handlers>
-            <filters>
-                <response-header name="server-header" header-name="Server" header-value="JBoss-EAP/7"/>
-                <response-header name="x-powered-by-header" header-name="X-Powered-By" header-value="Undertow/1"/>
 
+            <filters>
                 <response-header name="Access-Control-Allow-Origin" header-name="Access-Control-Allow-Origin" header-value="http://localhost:3000"/>
                 <response-header name="Access-Control-Allow-Methods" header-name="Access-Control-Allow-Methods" header-value="GET, POST, OPTIONS"/>
                 <response-header name="Access-Control-Allow-Headers" header-name="Access-Control-Allow-Headers" header-value="Authorization , Content-Type"/>
-                <response-header name="Access-Control-Allow-Credentials" header-name="Access-Control-Allow-Credentials" header-value="true"/>  
-
+                <response-header name="Access-Control-Allow-Credentials" header-name="Access-Control-Allow-Credentials" header-value="true"/>
             </filters>
-        </subsystem>
 
+        </subsystem>
+```
 2. Verify that the Loan Application Decision Service is up and running here:
 
 
--- decisionServerClient.js
+++ decisionServerClient.js
@@ -2,11 +2,11 @@
 angular.module('demo', [])
 .controller('kie-server-info', function($scope, $http) {
 
-    $http.get('http://localhost:8080/kie-server/services/rest/server', {
-        headers: {'accept' : 'application/json', 'content-type' : 'application/json' ,'Authorization': 'Basic ' + btoa('dsUser:dsUser1!') }
+    $http.get('http://' + ENV.kieserver_host + ':' + ENV.kieserver_port + ENV.kierserver_contextroot + '/services/rest/server', {
+        headers: {'accept' : 'application/json', 'content-type' : 'application/json' ,'Authorization': 'Basic ' + btoa(ENV.kieserver_user + ':' + ENV.kieserver_password) }
     }).
         then(function(response) {
-            
+
             $scope.greeting = response.data;
             $scope.info = $scope.greeting.result["kie-server-info"];
             console.log($scope.result);
@@ -18,48 +18,54 @@ angular.module('demo', [])
 
     $scope.applicationResult = "";
     $scope.applicationResultIcon = "";
-    $scope.displayResults = "display: none;"; 
+    $scope.displayResults = "display: none;";
 
     $scope.fireRules = function(){
-        console.log("fireRules! ");  
-        $http.post('http://localhost:8080/kie-server/services/rest/server/containers/instances/loan-application', '{ \"lookup\": \"default-stateless-ksession\", \"commands\": [ { \"insert\": { \"object\": { \"com.redhat.demo.qlb.loan_application.model.Applicant\": { \"creditScore\":'+$scope.applicant.creditScore+', \"name\":\"'+$scope.applicant.name+'\", \"age\":'+$scope.applicant.age+', \"yearlyIncome\":'+$scope.applicant.yearlyIncome+' } }, \"out-identifier\":\"applicant\" } }, { \"insert\": { \"object\": { \"com.redhat.demo.qlb.loan_application.model.Loan\": { \"amount\":'+$scope.loan.amount+', \"duration\":'+$scope.loan.duration+' } }, \"out-identifier\":\"loan\" } }, { \"start-process\" : { \"processId\" : \"loan-application.loan-application-decision-flow\", \"parameter\" : [ ], \"out-identifier\" : null } } ]}' ,{
-        headers: {'Authorization': 'Basic ' + btoa('dsUser:dsUser1!') }
+        console.log("fireRules! ");
+        console.log("KIE-Server host: " + ENV.kieserver_host);
+        console.log("KIE-Server port:" + ENV.kieserver_port);
+        console.log("KIE-Server ContextRoot: " + ENV.kieserver_contextroot);
+        console.log("KIE-Server containerId: " + ENV.kieserver_containerId);
+        console.log("User: " + ENV.kieserver_user);
+        console.log("Password: " + ENV.kieserver_password );
+        $http.post('http://' + ENV.kieserver_host + ':' + ENV.kieserver_port + ENV.kieserver_contextroot +  '/services/rest/server/containers/instances/' + ENV.kieserver_containerId, '{ \"lookup\": \"default-stateless-ksession\", \"commands\": [ { \"insert\": { \"object\": { \"com.redhat.demo.qlb.loan_application.model.Applicant\": { \"creditScore\":'+$scope.applicant.creditScore+', \"name\":\"'+$scope.applicant.name+'\", \"age\":'+$scope.applicant.age+', \"yearlyIncome\":'+$scope.applicant.yearlyIncome+' } }, \"out-identifier\":\"applicant\" } }, { \"insert\": { \"object\": { \"com.redhat.demo.qlb.loan_application.model.Loan\": { \"amount\":'+$scope.loan.amount+', \"duration\":'+$scope.loan.duration+' } }, \"out-identifier\":\"loan\" } }, { \"start-process\" : { \"processId\" : \"loan-application.decision-flow\", \"parameter\" : [ ], \"out-identifier\" : null } } ]}' ,{
+        headers: {'Authorization': 'Basic ' + btoa(ENV.kieserver_user + ':' + ENV.kieserver_password) }
         }).
             then(function(response) {
-                
+
                 $scope.greeting = response.data;
                 // $scope.info = $scope.greeting.result["kie-server-info"];
                // console.log($scope.greeting);
                 var approved =  $scope.greeting.result["execution-results"].results[0].value["com.redhat.demo.qlb.loan_application.model.Loan"].approved;
                 if (approved) {
-                    // approved 
+                    // approved
                     console.log("approved");
                     // change label to approved
-                    $scope.displayResults = ""; 
+                    $scope.displayResults = "";
                     $scope.applicationResult = "Approved";
                     $scope.applicationResultIcon = "pficon pficon-ok";
-                    // add comment to message 
+                    // add comment to message
                     var comment =  $scope.greeting.result["execution-results"].results[0].value["com.redhat.demo.qlb.loan_application.model.Loan"].comment;
-                    // add interestRate to message 
+                    // add interestRate to message
                     var interestRate =  $scope.greeting.result["execution-results"].results[0].value["com.redhat.demo.qlb.loan_application.model.Loan"].interestRate;
-                    // add monthlyRepayment to message 
+                    // add monthlyRepayment to message
                     var monthlyRepayment =  $scope.greeting.result["execution-results"].results[0].value["com.redhat.demo.qlb.loan_application.model.Loan"].monthlyRepayment;
                     $scope.applicationResultMessages = [comment,"Interest Rate : "+interestRate, "Monthly Repayment : "+monthlyRepayment];
                     console.log($scope.applicationResultMessages);
                 } else {
                     console.log("rejected");
-                    // rejected 
+                    // rejected
                     // change label to rejected
                     $scope.displayResults = "";
                     $scope.applicationResult = "Rejected";
                     $scope.applicationResultIcon = "pficon pficon-error-circle-o";
-                    // add comment to message 
+                    // add comment to message
                     var comment =  $scope.greeting.result["execution-results"].results[0].value["com.redhat.demo.qlb.loan_application.model.Loan"].comment;
                     $scope.applicationResultMessages = [comment];
                     console.log($scope.applicationResultMessages);
                 }
             });
-        
+
     }
 
 
