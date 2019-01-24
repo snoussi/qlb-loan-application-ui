708d7ce3e834d77aeda091ca1be191e430bc4c34
Rachid Snoussi <rsnoussi@redhat.com>
2019-01-24 17:36:15 +0100

    updated for RHDM 7.2

--- config/config.js
+++ config/config.js
@@ -5,6 +5,6 @@ var ENV = {
     'kieserver_port' : '8080',
     'kieserver_contextroot' : '/kie-server',
     'kieserver_containerId' : 'loan-application',
-    'kieserver_user' : 'kieserver',
-    'kieserver_password' : 'kieserver1!'
+    'kieserver_user' : 'dsUser',
+    'kieserver_password' : 'dsUser1!'
 };
